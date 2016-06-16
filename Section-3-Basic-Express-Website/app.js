4d1ce401b2288e4b6f9821438bcd20bb6331703e
Celso Mireles <celso@digitalstrategy.tips>
2016-06-15 20:24:05 -0700

    Page Routes & Views

--- Section-3-Basic-Express-Website/app.js
+++ Section-3-Basic-Express-Website/app.js
@@ -2,16 +2,24 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
+var pug = require('pug');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req,res){
-	console.log('Hello World');
-	res.send('Hello World..');
-})
+	res.render('index', {title: "Welcome"});
+});
+
+app.get('/about', function(req,res){
+	res.render('about');
+});
 
 app.listen(3000);
 console.log('Server is running on port 3000...');
